2e3874c0a38e17ba45bd2a00659c55208c6cd754
Martin Chaov <martin.c@sbtech.com>
2019-07-06 16:52:32 +0300

    simple stack implementation with JS private fields

---
+++ simple-data-structures/stack/stack.js
@@ -0,0 +1,56 @@
+class Stack {
+    #data;
+    #maxSize; // 0 = auto sizing
+
+    constructor(maxSize) {
+        this.#maxSize = typeof maxSize === "undefined" ? 0 : maxSize;
+        this.clear();
+    }
+
+    get count() { return this.#data.length; }
+
+    push(element) {
+        if (this.#maxSize !== 0 && this.count > this.#maxSize) {
+            throw new Error("stack overflow :)");
+        }
+        this.#data.push(element);
+    }
+
+    clear() { this.#data = new Array(0); }
+
+    contains(element) {
+        return this.#data.indexOf(element) > -1;
+    }
+
+    peek() {
+        return this.#data.slice();
+    }
+
+    pop() {
+        if (this.count <= 0) {
+            throw new Error("stack underflow :)");
+        }
+        this.#data.splice(this.count - 1, 1);
+    }
+}
+
+var s = new Stack(2);
+
+s.push(1);
+s.push(2);
+s.push(3);
+
+console.log(
+    s.count,
+    s.contains(1),
+    s.contains(4)
+)
+
+s.pop();
+
+console.log(
+    s.count,
+    s.contains(3)
+)
+
+console.log(s.peek());
